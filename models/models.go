4b9fa1c5d9fed12ea251e1e5ada01d81d89abf90
Fran Benet <fran.benet@gmail.com>
2024-09-03 17:34:27 +0300

    Update Post, Add Minipost and Data struct

--- models/models.go
+++ models/models.go
@@ -9,12 +9,20 @@ type User struct {
 	Password string
 }
 
+// Used to compile all the data for each post
 type Post struct {
-	Id      int
-	UserId  int
-	Title   string
-	Content string
-	Created string
+	Id            int
+	UserId        int
+	Title         string
+	Content       string
+	Created       string
+	Username      string
+	Category      string
+	LikesCount    int
+	CommentsCount int
+	LikeState     bool
+	DislikeState  bool
+	Avatar        string
 }
 
 type Category struct {
@@ -24,4 +32,21 @@ type Category struct {
 
 func (p Post) String() string {
 	return fmt.Sprintf("** %s **\n%s\n%s\n", p.Title, p.Content, p.Created)
+}
+
+// Used to compile all the dynamic data for the HTML
+type Data struct {
+	Categories  []string
+	Posts       []Post
+	TotalPages  int
+	CurrentPage int
+	MiniPosts   []MiniPost
+}
+
+// Used to compile all the dynamic data for each minipost in the sidebar
+type MiniPost struct {
+	Id         int
+	Username   string
+	Title      string
+	LikesCount int
 }
