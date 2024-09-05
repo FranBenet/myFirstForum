aa1a2300ad9e8ed295cef219b48a39238eeab82c
Fran Benet <fran.benet@gmail.com>
2024-09-05 12:46:29 +0300

    Update RenderTemplate func to accept all type of data and page name to be executed

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -8,34 +8,26 @@ import (
 	"gitea.koodsisu.fi/josepfrancescbenetmorella/literary-lions/models"
 )
 
-func RenderTemplate(w http.ResponseWriter, name string, data *models.Data) {
+func RenderTemplate(w http.ResponseWriter, name string, data interface{}) {
 	htmlTemplates := []string{
 		"web/templates/base.html",
 		"web/templates/header.html",
-		"web/templates/home.html",
-		"web/templates/posts-section.html",
-		"web/templates/post-templates.html",
-		"web/templates/post-view.html",
-		"web/templates/breadcrumbs.html",
 		"web/templates/sidebar.html",
-		"web/templates/login.html",
-		"web/templates/register.html",
-		"web/templates/user-profile.html",
-		"web/templates/notifications.html",
-		"web/templates/post-create.html",
+		"web/templates/breadcrumbs.html",
+		"web/templates/filter.html",
+		"web/templates/main-gallery.html",
+		"web/templates/post-templates.html",
+		"web/templates/pagination.html",
 	}
 
-	tmpl, err := template.ParseFiles(htmlTemplates...)
-	if err != nil {
-		log.Println("Error Parsing Template: %v\n", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
+	//	Adding to the Templates the needed html page to be sent for each specific page request.
+	htmlTemplates = append(htmlTemplates, "web/templates/"+name+".html")
 
-	err = tmpl.ExecuteTemplate(w, name, data)
+	tmpl := template.Must(template.ParseFiles(htmlTemplates...))
+
+	err := tmpl.ExecuteTemplate(w, name, data)
 	if err != nil {
-		log.Println("Error Executing Template: %v\n", err)
+		log.Printf("Error Executing Template: %v\n", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
@@ -43,13 +35,30 @@ func RenderTemplate(w http.ResponseWriter, name string, data *models.Data) {
 }
 
 // THIS FUNCTIONS HAS TO BE DELETED. IT'S JUST TO CREATE A SIMPLE DATA FOR DEVELOPING
-func GetData() *models.Data {
+func GetDataExample() *models.MainData {
+	user01 := &models.User{
+		Id:     1,
+		Name:   "HistoricalFictionBuff",
+		Avatar: "meerkat.png",
+	}
+	category01 := &models.Category{
+		Name: "Fantasy",
+	}
+	category02 := &models.Category{
+		Name: "Epic",
+	}
+	category03 := &models.Category{
+		Name: "Trilogy",
+	}
+	var categories01 []models.Category
+	categories01 = append(categories01, *category01)
+	categories01 = append(categories01, *category02)
+	categories01 = append(categories01, *category03)
 
 	post01 := &models.Post{
 		Id:            1,
-		UserId:        1,
-		Username:      "HistoricalFictionBuff",
-		Category:      "Fantasy",
+		User:          *user01,
+		Categories:    categories01,
 		Title:         "Lord Of the rings, everything about the trilogy that changed the world",
 		Content:       "flksjadogaj ogsalj goaigj aog jaogj oajsgj aoligjao jgiodsja ogja jgoajo jgdsa ogjaoj",
 		Created:       "16 March 2024",
@@ -59,13 +68,31 @@ func GetData() *models.Data {
 		DislikeState:  false,
 	}
 
+	user02 := &models.User{
+		Id:     2,
+		Name:   "BookEater",
+		Avatar: "bear.png",
+	}
+	category04 := &models.Category{
+		Name: "War",
+	}
+	category05 := &models.Category{
+		Name: "Shocking",
+	}
+	category06 := &models.Category{
+		Name: "Romantic",
+	}
+	var categories02 []models.Category
+	categories02 = append(categories02, *category04)
+	categories02 = append(categories02, *category05)
+	categories02 = append(categories02, *category06)
+
 	post02 := &models.Post{
 		Id:            2,
-		UserId:        2,
-		Username:      "BookEater",
-		Category:      "War",
+		User:          *user02,
+		Categories:    categories02,
 		Title:         "The Impact of Setting on 'All the Light We Cannot See",
-		Content:       "The settings in All the Light We Cannot See—particularly the war-torn cities of France—play such a crucial role in shaping the narrative. The way Anthony Doerr describes Saint-Malo almost makes it feel like a character itself.",
+		Content:       "The settings in All the Light We Cannot See—particularly the war-torn cities of France—play such a crucial role in shaping the narrative. The way Anthony Doerr describes Saint-Malo almost makes it feel like a character itself.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 		Created:       "21 November 2022",
 		LikesCount:    9,
 		CommentsCount: 9,
@@ -73,16 +100,23 @@ func GetData() *models.Data {
 		DislikeState:  false,
 	}
 
+	user03 := &models.User{
+		Name: "BookLover90",
+	}
+
 	minipost01 := &models.MiniPost{
 		Id:         1,
-		Username:   "BookLover90",
+		User:       *user03,
 		Title:      "Exploring the Depths of Character Development in 'To Kill a Mockingbird",
 		LikesCount: 10,
 	}
 
+	user04 := &models.User{
+		Name: "MarkKing",
+	}
 	minipost02 := &models.MiniPost{
 		Id:         2,
-		Username:   "FantasyFantastic",
+		User:       *user04,
 		Title:      "Comparing Magic Systems: Sanderson vs. Rowling",
 		LikesCount: 7,
 	}
@@ -97,7 +131,7 @@ func GetData() *models.Data {
 
 	categories := []string{"Fantasy", "War", "Fiction", "Non-fiction", "Romance", "Crime"}
 
-	data := &models.Data{
+	data := &models.MainData{
 		Categories: categories,
 		Posts:      postsCollection,
 		MiniPosts:  minipostsCollection,
@@ -107,27 +141,78 @@ func GetData() *models.Data {
 }
 
 // THIS FUNCTIONS HAS TO BE DELETED. IT'S JUST TO CREATE A SIMPLE DATA FOR DEVELOPING
-func getPostId() *models.Data {
+func GetPostIdExample() *models.Post {
+	user03 := &models.User{
+		Id:     1,
+		Name:   "HistoricalFictionBuff",
+		Avatar: "meerkat.png",
+	}
+
+	category01 := &models.Category{
+		Name: "Fantasy",
+	}
+
+	category02 := &models.Category{
+		Name: "Epic",
+	}
+	category03 := &models.Category{
+		Name: "Trilogy",
+	}
+
+	var categories03 []models.Category
+	categories03 = append(categories03, *category01)
+	categories03 = append(categories03, *category02)
+	categories03 = append(categories03, *category03)
+
+	user01 := &models.User{
+		Id:     34,
+		Name:   "MarkKing",
+		Avatar: "dog2.png",
+	}
+
+	comment01 := &models.Comment{
+		Id:           1,
+		Content:      "I've been re-reading To Kill a Mockingbird, and I'm amazed by how Harper Lee crafted such complex and relatable characters. Especially Atticus Finch—his moral compass and calm demeanor really resonate with me. How do you think Lee's portrayal of these characters reflects the social issues of the time? Also, who is your favorite character and why? Would love to hear your thoughts! /n I've been re-reading To Kill a Mockingbird, and I'm amazed by how Harper Lee crafted such complex and relatable characters. Especially Atticus Finch—his moral compass and calm demeanor really resonate with me. How do you think Lee's portrayal of these characters reflects the social issues of the time? Also, who is your favorite character and why? Would love to hear your thoughts!",
+		Created:      "23 March 2026",
+		User:         *user01,
+		LikesCount:   21,
+		LikeState:    true,
+		DislikeState: false,
+	}
+
+	user02 := &models.User{
+		Id:     38,
+		Name:   "DeboraBooks",
+		Avatar: "rabbit.png",
+	}
+
+	comment02 := &models.Comment{
+		Id:           2,
+		Content:      "SHIT SHIT SHIT shit shit shit what ever text may seem important here!",
+		Created:      "2 April 2026",
+		User:         *user02,
+		LikesCount:   2,
+		LikeState:    false,
+		DislikeState: true,
+	}
+
+	var commentsCollection []models.Comment
+	commentsCollection = append(commentsCollection, *comment01)
+	commentsCollection = append(commentsCollection, *comment02)
+
 	post01 := &models.Post{
 		Id:            1,
-		UserId:        1,
-		Username:      "HistoricalFictionBuff",
-		Category:      "Fantasy",
+		Categories:    categories03,
 		Title:         "Lord Of the rings, everything about the trilogy that changed the world",
-		Content:       "flksjadogaj ogsalj goaigj aog jaogj oajsgj aoligjao jgiodsja ogja jgoajo jgdsa ogjaoj",
+		Content:       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 		Created:       "16 March 2024",
+		User:          *user03,
 		LikesCount:    5,
 		CommentsCount: 3,
 		LikeState:     false,
 		DislikeState:  false,
-		Avatar:        "meerkat.png",
+		Comments:      commentsCollection,
 	}
 
-	var postsCollection []models.Post
-	postsCollection = append(postsCollection, *post01)
-
-	data := &models.Data{
-		Posts: postsCollection,
-	}
-	return data
+	return post01
 }
