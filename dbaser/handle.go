64eeacb3365676406be2ca86465d4bfc4f062639
uamoti <uamoti@anche.no>
2024-09-18 09:28:09 +0300

    Update DbHandle use of path

--- dbaser/handle.go
+++ dbaser/handle.go
@@ -2,11 +2,12 @@ package dbaser
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 )
 
 func DbHandle(path string) (*sql.DB, error) {
-	db, err := sql.Open("sqlite3", "./forum.db")
+	db, err := sql.Open("sqlite3", fmt.Sprintf("file:%s", path))
 	if err != nil {
 		log.Println("Error opening database file")
 		return nil, err
