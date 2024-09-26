e5a57aa9ebfaefeb94a4ee2da798da82353b3c09
uamoti <uamoti@anche.no>
2024-09-26 14:31:51 +0300

    Update AddCategory, add CategoryExists, CategoryIdByName, AddPostCategories
    AddCategory now takes a string as parameter instead of a models.PostCategory.
    CategoryExists is needed as the user is able to provide new categories, so I need to check if they're already in the DB.
    The front is sending me the names of the categories, so I need to get their IDs to connect the post and the categories.
    AddPostCategories is a wrapper for adding multiple categories, as the front is sending me a slice of category names.

--- dbaser/categories.go
+++ dbaser/categories.go
@@ -62,16 +62,62 @@ func AddPostCategory(db *sql.DB, category models.PostCategory) error {
 	return nil
 }
 
-func AddCategory(db *sql.DB, category models.Category) (int, error) {
+func AddCategory(db *sql.DB, name string) (int, error) {
 	stmt, err := db.Prepare("insert into categories (label) values (?)")
 	if err != nil {
 		return 0, err
 	}
 	defer stmt.Close()
-	res, err := stmt.Exec(category.Name)
+	res, err := stmt.Exec(name)
 	if err != nil {
 		return 0, err
 	}
 	id, err := res.LastInsertId()
 	return int(id), nil
+}
+
+func CategoryExists(db *sql.DB, name string) (bool, error) {
+	row := db.QueryRow("select * from categories where label=?", name)
+	var label string
+	if err := row.Scan(&label); err == sql.ErrNoRows {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func CategoryIdByName(db *sql.DB, name string) (int, error) {
+	row := db.QueryRow("select id from categories where label=?", name)
+	var id int
+	if err := row.Scan(&id); err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func AddPostCategories(db *sql.DB, categories []string, postId int) error {
+	for _, cat := range categories {
+		exists, err := CategoryExists(db, cat)
+		if err != nil {
+			return err
+		}
+		if exists { // The category is present in the database.
+			id, err := CategoryIdByName(db, cat)
+			if err != nil {
+				return err
+			}
+			err = AddPostCategory(db, models.PostCategory{PostId: postId, CategoryId: id})
+			if err != nil {
+				return err
+			}
+		} else { // New category added by the user.
+			id, err := AddCategory(db, cat)
+			if err != nil {
+				return err
+			}
+			err = AddPostCategory(db, models.PostCategory{PostId: postId, CategoryId: id})
+		}
+	}
+	return nil
 }
